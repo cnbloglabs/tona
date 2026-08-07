20468658b94350ed743699d6600900bcbc46ed31
guangzan <guangzan1999@outlook.com>
2026-08-07 20:51:20 +0800

    feat(theme-view): migrate view theme into monorepo

---
+++ themes/view/vite.config.ts
@@ -0,0 +1,12 @@
+import tona from 'tona-vite'
+import { defineConfig } from 'vite-plus'
+
+export default defineConfig({
+  plugins: [
+    tona({
+      themeName: 'view',
+      inlineCss: true,
+      hash: false,
+    }),
+  ],
+})
